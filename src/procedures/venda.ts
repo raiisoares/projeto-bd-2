e59473a47eb65e9fb723cd7dbde9bb024896d147
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2024-06-02 16:07:43 -0300

    Create venda.ts

---
+++ src/procedures/venda.ts
@@ -0,0 +1,24 @@
+export function venda() {
+  return `
+
+    CREATE PROCEDURE Venda(IN id_product INT)
+    BEGIN
+        DECLARE product_quantity INT;
+        
+        -- Get the current quantity of the product
+        SELECT quantity INTO product_quantity FROM products WHERE id = id_product;
+        
+        -- Check if the product exists and has available quantity
+        IF product_quantity IS NOT NULL AND product_quantity > 0 THEN
+
+            -- Reduce the quantity of the product by 1
+            UPDATE products SET quantity = quantity - 1 WHERE id = id_product;
+            
+            SELECT CONCAT('Sale successful! Product ', id_product, ' quantity reduced by 1.') AS Result;
+            
+        ELSE
+            SELECT 'Error: Product not exist!' AS Result;
+        END IF;
+        
+      END;`
+}
