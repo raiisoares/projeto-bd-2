470e5d6efb25dd0f4ace4abcc1adb3ff0de56473
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2024-06-02 16:26:29 -0300

    Update reajuste.ts

--- src/procedures/reajuste.ts
+++ src/procedures/reajuste.ts
@@ -1,18 +1,19 @@
 export function reajuste() {
   return `
 
-    CREATE PROCEDURE AdjustSalary(IN percentage DECIMAL(5,2), IN category VARCHAR(50))
+    CREATE PROCEDURE AdjustSalary(IN percentage DECIMAL(5,2), IN position VARCHAR(50))
     BEGIN
         DECLARE done BOOLEAN DEFAULT FALSE;
         
-        -- Check if the category exists
+        -- Check if the position exists
         DECLARE categoryExists INT DEFAULT 0;
-        SELECT COUNT(*) INTO categoryExists FROM employees WHERE category = category;
+        
+        SELECT COUNT(*) INTO categoryExists FROM employee WHERE position = position;
         
         IF categoryExists > 0 THEN
             
-            -- Update salary for employees in the category
-            UPDATE employees SET salary = salary * (1 + percentage / 100) WHERE category = category;
+            -- Update salary for employee in the category
+            UPDATE employee SET salary = salary * (1 + percentage / 100) WHERE position = position;
             SET done = TRUE;
         
         ELSE
