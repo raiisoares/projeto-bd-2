aaf7232d9fc072c7a242f502a3bd64022568e151
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2024-06-04 10:57:17 -0300

    Create vendas por cliente.ts

---
+++ src/views/vendas por cliente.ts
@@ -0,0 +1,9 @@
+export function SalesByCustomer  () {
+  return `
+    CREATE VIEW SalesByCustomer AS
+    SELECT c.name AS Customer, SUM(s.amount) AS TotalSales
+    FROM customer c
+    JOIN sale s ON c.id = s.id_customer
+    GROUP BY c.name;
+  END;`
+}
